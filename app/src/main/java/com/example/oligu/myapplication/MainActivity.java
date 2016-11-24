0fd4eb7f65a372a0be2dbb1386141c789743e341
OliverGuth <oliguth@web.de>
2016-11-24 22:09:29 +0100

    Initial Commit 2

---
+++ app/src/main/java/com/example/oligu/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.oligu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testUpload()
+    {
+        int a = 1+1;
+    }
+}
